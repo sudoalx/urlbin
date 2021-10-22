12bec6ddb2b0de003f3fd93ac8b4187c530c3ffe
Alex <nalexcastaneda@gmail.com>
2021-10-22 15:29:13 -0500

    detect http and redirect to https

--- public/js/script.js
+++ public/js/script.js
@@ -24,3 +24,6 @@ function snackbar(label) {
     // After 3 seconds, remove the show class from DIV
     setTimeout(function () { x.className = x.className.replace("show", ""); }, 5000);
 }
+if (location.protocol !== 'https:') {
+    location.replace(`https:${location.href.substring(location.protocol.length)}`);
+}
